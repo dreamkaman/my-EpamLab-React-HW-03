6351611efcda1fab9e7df1c860342e0042343bf1
Dreamkaman <riabushko@gmail.com>
2023-01-31 00:51:01 +0200

    Next saga

---
+++ src/redux/store/saga/index.ts
@@ -0,0 +1,8 @@
+import { takeEvery } from 'redux-saga/effects';
+import { userLogin, userLogout } from '../user/actionCreators';
+
+export function* workerSaga() {}
+
+export function* watcherSaga() {}
+
+export default function* rootSaga() {}
