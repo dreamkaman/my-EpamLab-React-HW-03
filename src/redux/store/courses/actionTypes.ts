be21d50f03a5b30c2cf6b2b72c914c3866b78d03
Dreamkaman <riabushko@gmail.com>
2023-02-10 01:34:49 +0200

    Next saga

--- src/redux/store/courses/actionTypes.ts
+++ src/redux/store/courses/actionTypes.ts
@@ -1,3 +1,5 @@
 export const GET_COURSES = 'GET_COURSES';
 export const SET_COURSES = 'SET_COURSES';
 export const CLEAR_COURSES = 'CLEAR_COURSES';
+export const ADD_COURSE = 'ADD_COURSE';
+export const DELETE_COURSE = 'DELETE_COURSE';
--- src/redux/store/courses/reducer.ts
+++ src/redux/store/courses/reducer.ts
@@ -1,5 +1,10 @@
 import { createReducer } from '@reduxjs/toolkit';
-import { setAllCoursesAction, clearAllCoursesAction } from './actionCreators';
+import {
+	setAllCoursesAction,
+	clearAllCoursesAction,
+	addNewCourseAction,
+	deleteCourseAction,
+} from './actionCreators';
 
 import { ICourse } from 'Context';
 
@@ -11,5 +16,15 @@ export const coursesReducer = createReducer(initialReducer, {
 	},
 	[clearAllCoursesAction.type]: () => {
 		return initialReducer;
+	},
+	[addNewCourseAction.type]: (state, action) => {
+		return [...state, ...action.payload];
+	},
+	[deleteCourseAction.type]: (state, action) => {
+		const filteredCourses = state.filter(
+			(course) => course.id !== action.payload //in this case we pass only id as payload
+		);
+
+		return [...filteredCourses];
 	},
 });
