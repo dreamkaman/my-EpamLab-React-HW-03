19d09f12feb13ebb497f9cb1220d9e6965722aab
Dreamkaman <riabushko@gmail.com>
2023-02-11 00:19:34 +0200

    Next saga

--- src/redux/store/authors/actionCreators.ts
+++ src/redux/store/authors/actionCreators.ts
@@ -1,8 +1,15 @@
 import { createAction } from '@reduxjs/toolkit';
-import { GET_AUTHORS, SET_AUTHORS, CLEAR_AUTHORS } from './actionTypes';
+import {
+	GET_AUTHORS,
+	SET_AUTHORS,
+	CLEAR_AUTHORS,
+	ADD_NEW_AUTHOR,
+} from './actionTypes';
 
 export const getAllAuthorsAction = createAction(GET_AUTHORS);
 
 export const setAllAuthorsAction = createAction(SET_AUTHORS);
 
 export const clearAllAuthorsAction = createAction(CLEAR_AUTHORS);
+
+export const addNewAuthorAction = createAction(ADD_NEW_AUTHOR);
